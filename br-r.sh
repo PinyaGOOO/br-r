b06d37a2b3e65f7e4b3d65eb31d9ffeb19787377
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-08 21:21:58 +0300

    Add files via upload

--- br-r.sh
+++ br-r.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.addresses 2024:2::2/64
-nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.gateway 2024:2::1
+nmcli con modify Проводное\ подключение\ 1 ipv6.gateway 2024:2::1
 nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 2.2.2.2/30
-nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.gateway 2.2.2.1
+nmcli con modify Проводное\ подключение\ 1 ipv4.gateway 2.2.2.1
 
 nmcli con modify Проводное\ подключение\ 2 ipv6.method manual ipv6.addresses FD24:192::1/124
 nmcli con modify Проводное\ подключение\ 2 ipv4.method manual ipv4.addresses 192.168.100.1/28
 
+echo -e "net.ipv4.ip_forward=1\nnet.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+sysctl-p
+
+dnf install -y nftables
+echo -e 'table inet mynat {\n\tchain my_masquerade {\n\ttype nat hook postrouting priority srcnat;\n\toifname "ens18" masquerade\n\t}\n}' > /etc/nftables/isp.nft
+echo 'include "/etc/nftables/br-r.nft"' >> /etc/sysconfig/nftables.conf
+systemctl enable --now nftables
+
+nmcli con add type ip-tunnel ifname tun1 mode gre remote 1.1.1.2 local 2.2.2.2
+nmcli con modify ip-tunnel-tun1 ipv4.method manual ipv4.addresses 10.10.10.2/30
+nmcli con modify ip-tunnel-tun1 ipv6.method manual ipv6.addresses FD24:10::2/64
+nmcli connection modify ip-tunnel-tun1 ip-tunnel.ttl 64
+sed -i '11i\parent=ens18' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
+sed -i '/id=ip-tunnel-tun1/d' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
+sed -i '2i\id=tun1' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
 
 
 
