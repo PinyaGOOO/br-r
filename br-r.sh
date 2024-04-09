52afdfcf4559d42d9998aeb3b4144f7eb644be44
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-09 12:29:10 +0300

    Add files via upload

--- br-r.sh
+++ br-r.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.addresses 2024:2::2/64
-nmcli con modify Проводное\ подключение\ 1 ipv6.gateway 2024:2::1
-nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 2.2.2.2/30
-nmcli con modify Проводное\ подключение\ 1 ipv4.gateway 2.2.2.1
+nmcli con modify ens18 ipv6.method manual ipv6.addresses 2024:2::2/64
+nmcli con modify ens18 ipv6.gateway 2024:2::1
+nmcli con modify ens18 ipv4.method manual ipv4.addresses 2.2.2.2/30
+nmcli con modify ens18 ipv4.gateway 2.2.2.1
+nmcli con modify ens18 ipv4.dns 8.8.8.8
 
-nmcli con modify Проводное\ подключение\ 2 ipv6.method manual ipv6.addresses FD24:192::1/124
-nmcli con modify Проводное\ подключение\ 2 ipv4.method manual ipv4.addresses 192.168.100.1/28
+nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.addresses FD24:192::1/124
+nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 192.168.100.1/28
 
 echo -e "net.ipv4.ip_forward=1\nnet.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
 sysctl -p
