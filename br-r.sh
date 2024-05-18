d3fd574c5e6a8bac37c051ebd5a137a249fa5a9e
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-05-18 12:53:19 +0300

    Add files via upload

--- br-r.sh
+++ br-r.sh
@@ -3,17 +3,14 @@ dnf remove -y git
 dnf install -y frr
 dnf install -y nftables
 
-nmcli con modify ens18 ipv6.method manual ipv6.addresses 2024:2::2/64
-nmcli con modify ens18 ipv6.gateway 2024:2::1
 nmcli con modify ens18 ipv4.method manual ipv4.addresses 2.2.2.2/30
 nmcli con modify ens18 ipv4.gateway 2.2.2.1
 nmcli con modify ens18 ipv4.dns 8.8.8.8
-nmcli con modify ens18 ipv6.dns 2001:4860:4860::8888
 
-nmcli con modify Проводное\ подключение\ 1 ipv6.method manual ipv6.addresses FD24:192::1/124
+
 nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 192.168.100.1/28
 
-echo -e "net.ipv4.ip_forward=1\nnet.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+echo -e "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
 echo -e 'table inet my_nat {\n\tchain my_masquerade {\n\ttype nat hook postrouting priority srcnat;\n\toifname "ens18" masquerade\n\t}\n}' > /etc/nftables/br-r.nft
@@ -22,7 +19,6 @@ systemctl enable --now nftables
 
 nmcli con add type ip-tunnel ifname tun1 mode gre remote 1.1.1.2 local 2.2.2.2
 nmcli con modify ip-tunnel-tun1 ipv4.method manual ipv4.addresses 10.10.10.2/30
-nmcli con modify ip-tunnel-tun1 ipv6.method manual ipv6.addresses FD24:10::2/64
 nmcli connection modify ip-tunnel-tun1 ip-tunnel.ttl 64
 sed -i '11i\parent=ens18' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
 sed -i '/id=ip-tunnel-tun1/d' /etc/NetworkManager/system-connections/ip-tunnel-tun1.nmconnection
@@ -46,17 +42,7 @@ vtysh -c "configure terminal" \
     -c "no ip ospf network broadcast" \
     -c "no ip ospf passive" \
     -c "exit" \
-    -c "do write" \
-    -c "router ospf6" \
-    -c "ospf6 router-id 2.2.2.2" \
-    -c "exit" \
-    -c "interface tun1" \
-    -c "ipv6 ospf6 area 0" \
-    -c "exit" \
-    -c "interface ens18" \
-    -c "ipv6 ospf6 area 0" \
-    -c "exit" \
-    -c "do write"
+    -c "do write" 
 
 useradd -c "Branch admin" Branch_admin -U
 echo "Branch_admin:P@ssw0rd" | chpasswd
